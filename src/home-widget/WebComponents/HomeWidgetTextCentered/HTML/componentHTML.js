e274907fc52f75d632bb8a40a67e5949562ebaa9
AlexKole113 <alexander.koledov@gmail.com>
2021-07-16 14:39:16 +0200

    add ThreeJs, GSAP in project. Create slider effect for home-widget WebComp

--- src/home-widget/WebComponents/HomeWidgetTextCentered/HTML/componentHTML.js
+++ src/home-widget/WebComponents/HomeWidgetTextCentered/HTML/componentHTML.js
@@ -2,13 +2,10 @@ import slide from "./slide.js";
 import socialLink from "./socialLink.js";
 
 const HTML = ( items , socialLinks ) => {
-    const slides = items.reduce(( accum, item ) => accum + slide( item ),'');
     const smLinks = socialLinks.reduce(( accum, item ) => accum + socialLink( item ),'');
     return (`
     <div class="home-widget-text-center">
-    <div class="home-widget-collection">
-        ${slides}
-    </div>
+    <div class="home-widget-collection"></div>
     <!--   SOCIAL LINKS  -->
     ${ (socialLinks?.length) ? 
         (`<div class="social-links">
--- src/home-widget/WebComponents/HomeWidgetTextCentered/index.js
+++ src/home-widget/WebComponents/HomeWidgetTextCentered/index.js
@@ -1,5 +1,6 @@
 import componentCSS from "./CSS/componentCSS.js";
 import componentHTML from "./HTML/componentHTML.js";
+import Slider from "../../scripts/Slider.js";
 export default class HomeWidgetTextCentered extends HTMLElement {
 
     static isNewValChanged = ( newVal, oldVal ) => newVal !== oldVal
@@ -13,10 +14,10 @@ export default class HomeWidgetTextCentered extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
-        const props = {
+        this.props = {
             data: JSON.parse( this.getAttribute('data' ) ),
         }
-        HomeWidgetTextCentered.template.innerHTML = HomeWidgetTextCentered.getHTML( props.data );
+        HomeWidgetTextCentered.template.innerHTML = HomeWidgetTextCentered.getHTML( this.props.data );
         this?.shadowRoot?.append( HomeWidgetTextCentered.template.content.cloneNode(true) );
 
         this.onmousedown = (event) => {
@@ -29,18 +30,68 @@ export default class HomeWidgetTextCentered extends HTMLElement {
         this.onmouseup = () => {
             this.removeAttribute('activate')
         }
+
     }
 
-
-
     connectedCallback() {
-            console.log(this)
+
+        this.sliderEffect = new Slider({
+            uniforms: {
+                intensity: {value: 1, type:'f', min:0., max:3}
+            },
+            images: this.props.data.items.map(({imageUrl}) => imageUrl),
+            container: this?.shadowRoot.querySelector('.home-widget-collection'),
+            fragment: `
+		uniform float time;
+		uniform float progress;
+		uniform float intensity;
+		uniform float width;
+		uniform float scaleX;
+		uniform float scaleY;
+		uniform float transition;
+		uniform float radius;
+		uniform float swipe;
+		uniform sampler2D texture1;
+		uniform sampler2D texture2;
+		uniform sampler2D displacement;
+		uniform vec4 resolution;
+		varying vec2 vUv;
+		mat2 getRotM(float angle) {
+		    float s = sin(angle);
+		    float c = cos(angle);
+		    return mat2(c, -s, s, c);
+		}
+		const float PI = 3.1415;
+		const float angle1 = PI *0.25;
+		const float angle2 = -PI *0.75;
+
+
+		void main()	{
+			vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
+
+			vec4 disp = texture2D(displacement, newUV);
+			vec2 dispVec = vec2(disp.r, disp.g);
+
+			vec2 distortedPosition1 = newUV + getRotM(angle1) * dispVec * intensity * progress;
+			vec4 t1 = texture2D(texture1, distortedPosition1);
+
+			vec2 distortedPosition2 = newUV + getRotM(angle2) * dispVec * intensity * (1.0 - progress);
+			vec4 t2 = texture2D(texture2, distortedPosition2);
+
+			gl_FragColor = mix(t1, t2, progress);
+
+		}
+
+	`
+        });
     }
 
     attributeChangedCallback( name, oldVal, newVal ) {
         switch ( name ) {
             case 'data':
-                if( HomeWidgetTextCentered.isNewValChanged( newVal, oldVal ) ) console.log( 'data changed' );
+                if( HomeWidgetTextCentered.isNewValChanged( newVal, oldVal ) ) {
+                    console.log( 'data changed' );
+                }
                 break;
         }
     }
